09872932cdfe29627a3457c62ade84f286778881
malbreitenbach <maltemarkusbreitenbach@stud.tu-darmstadt.de>
2018-05-21 19:22:55 +0300

    Initial push for HLP code

---
+++ src/AscTec_SDK_v3.0/protocol.c
@@ -0,0 +1,17 @@
+/*
+ * protocol.c
+ */
+
+#include "protocol.h"
+/**
+ * Refresh protocolStream with current sensor data.
+ * @param proByteStream 	ByteStream to be refreshed
+ * @param newData 			Struct including new data
+ */
+void refreshProtocolStream(protocol_u *proStream)
+{
+	proStream->protocol_s.angle_pitch = RO_ALL_Data.angle_pitch;
+	proStream->protocol_s.angle_roll = RO_ALL_Data.angle_roll;
+	proStream->protocol_s.angle_yaw = RO_ALL_Data.angle_yaw;
+}
+
