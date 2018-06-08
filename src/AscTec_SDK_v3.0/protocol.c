a5e240233f44af546865372c37c612ada1a41c7f
malbreitenbach <maltemarkusbreitenbach@stud.tu-darmstadt.de>
2018-06-08 17:48:45 +0300

    protocol stream adjustments, solution is tested as working

--- src/AscTec_SDK_v3.0/protocol.c
+++ src/AscTec_SDK_v3.0/protocol.c
@@ -7,13 +7,75 @@
 
 /**
  * Refresh protocolStream with current sensor data.
- * @param proByteStream 	ByteStream to be refreshed
- * @param newData 			Struct including new data
+ * @param proStream 	Protocol stream to be refreshed
  */
 void refreshProtocolStream(protocol_u *proStream)
 {
+	/* Sensor READ
 	proStream->protocol_s.angle_pitch = RO_ALL_Data.angle_pitch;
 	proStream->protocol_s.angle_roll = RO_ALL_Data.angle_roll;
 	proStream->protocol_s.angle_yaw = RO_ALL_Data.angle_yaw;
+	*/
+
+	//TETSTING SEQUENCE
+	proStream->protocol_s.startByte = 0x01;
+	proStream->protocol_s.angle_pitch = 1;
+	proStream->protocol_s.angle_roll = 2;
+	proStream->protocol_s.angle_yaw = 3;
+	proStream->protocol_s.test_int_1 = 4;
+	proStream->protocol_s.test_int_2 = 5;
+	proStream->protocol_s.test_int_3 = 6;
+	proStream->protocol_s.test_int_4 = 7;
+	proStream->protocol_s.test_int_5 = 8;
+	proStream->protocol_s.test_int_6 = 9;
+	proStream->protocol_s.test_int_7 = 10;
+	proStream->protocol_s.test_int_8 = 11;
+	proStream->protocol_s.test_int_9 = 12;
+	proStream->protocol_s.test_int_10 = 13;
+	proStream->protocol_s.test_int_11 = 14;
+	proStream->protocol_s.test_int_12 = 15;
+
+	proStream->protocol_s.test_u16_1 = 16;
+	proStream->protocol_s.test_u16_2 = 17;
+
+
+}
+
+/**
+ * Generate a checksum calculated from sensory data, stored at the end of protocol's stream. 
+ * @param proStream 	protocol stream the checksum will be calculated for
+ */
+void generateChecksum(protocol_u *proStream)
+{
+	//ToDo: Implement a CRC or a Fletchers's Checksum
+	
+	//Now used: Simple Additional Checksum
+	uint32_t checksum = 0;
+	for(uint8_t i = 0; i < num_sum-4; i++)
+		checksum += proStream->bytestream[i]; //Overflow is accepted
+	
+	//Store checksum in protocolByteStream
+	proStream->protocol_s.checksum = checksum;
+}
+
+/**
+ * Generate a checksum calculated from sensory data, stored at the end of protocol's stream. 
+ * @param proStream 	protocol stream to be checked for correct checksum
+ * @return '1' for correct checksum, '0' for incorrect checksum
+ */
+char checkChecksum(protocol_u *proStream)
+{
+	//ToDo: Implement a CRC or a Fletchers's Checksum
+	
+	//Now used: Simple Additional Checksum
+	uint8_t checksum = 0;
+	for(uint8_t i = 0; i < num_sum-4; i++)
+		checksum += proStream->bytestream[i]; //Overflow is accepted
+	
+	//Check if the calculated checksum equals the received checksum
+	if(checksum == proStream->protocol_s.checksum)
+		return 1;
+	else
+		return 0;
 }
 
--- src/AscTec_SDK_v3.0/protocol.h
+++ src/AscTec_SDK_v3.0/protocol.h
@@ -10,32 +10,49 @@
 
 /*	--- Protocol Parameter --- */
 #define num_8b 0
-#define num_16b 0
-#define num_32b 3
+#define num_16b 2
+#define num_32b 16
 #define num_64b 0
 
-#define num_sum ( num_8b + 2 * num_16b + 4 * num_32b + 8 * num_64b + 3)
+#define num_sum ( num_8b + 2 * num_16b + 4 * num_32b + 8 * num_64b + 1)
 
+#pragma pack(push,1)
 typedef union {
 	struct {
 		/*	--- Protocol head --- */
-		uint8_t startByte;
+		uint8_t startByte; //Status Flags; 0: Valid, 1: ...
+
+		uint16_t test_u16_1;
+		uint16_t test_u16_2;
 
 		/*	--- Sensory data --- */
 		int angle_pitch;
 		int angle_roll;
 		int angle_yaw;
+		int test_int_1;
+		int test_int_2;
+		int test_int_3;
+		int test_int_4;
+		int test_int_5;
+		int test_int_6;
+		int test_int_7;
+		int test_int_8;
+		int test_int_9;
+		int test_int_10;
+		int test_int_11;
+		int test_int_12;
 
 		/*	--- Protocol end --- */
-		uint8_t checksum;
-		uint8_t endByte;
-
+		uint32_t checksum;
 	} protocol_s;
 
 	uint8_t bytestream[num_sum];
 }protocol_u;
+#pragma pack(pop)
 
 void refreshProtocolStream(protocol_u *proStream);
+void generateChecksum(protocol_u *proStream);
+char checkChecksum(protocol_u *proStream);
 
 
 #endif /* PROTOCOL_H_ */
